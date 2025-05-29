2eaeac1cd6d0f2ef91df04e16eff8eb6459d3a83
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-05-29 16:34:13 +0200

    feat: add playGame function with random computer choice and result logic

---
+++ pages/rock-paper-scissors/script.js
@@ -0,0 +1,37 @@
+
+
+
+const choice = ["rock", "paper", "scissors"]; //Array
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice){
+
+    const computerChoise = choice[Math.floor( Math.random() * 3 )];
+    let result = "";
+
+    if(playerChoice === computerChoise ){
+        result = "IT'S A TIE";    
+    }
+
+    else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoise === "scissors") ? "YOU WIN" : "YOU LOSE";
+                break;
+            case "paper":
+                result = (computerChoise === "rock") ? "YOU WIN" : "YOU LOSE";
+                break;
+            case "scissors":
+                result = (computerChoise === "paper") ? "YOU WIN" : "YOU LOSE";
+                break;    
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoise}`;
+    resultDisplay.textContent = result;
+
+
+}

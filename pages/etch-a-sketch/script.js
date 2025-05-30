20249d77556eeaab16144a5401b348e2a597649e
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-05-30 23:57:50 +0200

    feat: added color selection and board reset features
    Implemented buttons to switch color modes (black, random, eraser), added colorCell function, enabled drawing via mousedown and mouseover, reset board functionality, updated 'Set Grid' to 'Set Board' and added placeholder to input

--- pages/etch-a-sketch/script.js
+++ pages/etch-a-sketch/script.js
@@ -2,18 +2,47 @@ const board = document.querySelector("#etch-board");
 const form = document.querySelector("#size-form");
 const input = document.querySelector("#grid-size");
 
+const blackBtn = document.querySelector("#black-btn");
+const randomBtn = document.querySelector("#random-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
+const resetBtn = document.querySelector("#reset-btn");
+
+let currentColor = "black";
+let resetState = false;
+let mouseDown = false;
+
+
 form.addEventListener("submit", getSize);
+blackBtn.addEventListener("click", () => currentColor = "black");
+randomBtn.addEventListener("click", () => currentColor = "random");
+eraserBtn.addEventListener("click", () => currentColor = "white");
+resetBtn.addEventListener("click", resetBoard);
+
+
+document.body.addEventListener("mousedown", () => mouseDown = true);
+document.body.addEventListener("mouseup", () => mouseDown = false);
+
+
 
 function createBoard(size){
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let numDiv = size * size;
-
     board.innerHTML = "";
 
     for (let i = 0; i < numDiv; i++){
-        let div = document.createElement("div");
+        const div = document.createElement("div");
+        div.classList.add("grid-square");
+
+        div.addEventListener("mousedown", () => {
+        colorCell(div);
+        });
+
+        div.addEventListener("mouseover", () => {
+        if (mouseDown) colorCell(div);
+        });
+
         board.insertAdjacentElement("beforeend", div)
     }
 }
@@ -28,4 +57,20 @@ function getSize(e){
     }
 
     createBoard(size);
+}
+
+function colorCell(cell) {
+    if (currentColor === "random") {
+        const hue = Math.floor(Math.random() * 360);
+        cell.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+    } else {
+        cell.style.backgroundColor = currentColor;
+    }
+}
+
+function resetBoard(){
+    const cells = document.querySelectorAll(".grid-square");
+    cells.forEach(cell => {
+        cell.style.backgroundColor = "white";
+    });
 }
